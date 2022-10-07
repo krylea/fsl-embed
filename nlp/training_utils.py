29d9fc62b615ce816afe3606a0670bfdbc603954
Kira Selby <kaselby@uwaterloo.ca>
2022-10-07 11:46:26 -0400

    nlp

--- nlp/training_utils.py
+++ nlp/training_utils.py
@@ -5,9 +5,9 @@ from transformers import Trainer
 class VQTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         if return_outputs:
-            loss, outputs = super().compute_loss(model, inputs, return_outputs)
+            loss, outputs = super(VQTrainer, self).compute_loss(model, inputs, return_outputs)
         else:
-            loss = super().compute_loss(model, inputs, return_outputs)
+            loss = super(VQTrainer, self).compute_loss(model, inputs, return_outputs)
         
         loss += model.bert.embeddings.word_embeddings.symbol_loss_buffer
 
